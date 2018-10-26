e53d6c1972f4421cc5650053215bcf0cde0d0b3c
Ziao Wang <ziao@bank.cs.columbia.edu>
2018-10-26 12:34:48 -0400

    123

--- webserver/server.py
+++ webserver/server.py
@@ -2,7 +2,7 @@
 import os
 from sqlalchemy import *
 from sqlalchemy.pool import NullPool
-from flask import Flask, request, render_template, g, redirect, Response
+from flask import Flask, request, render_template, g, redirect, Response, session
 
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask(__name__, template_folder=tmpl_dir)
